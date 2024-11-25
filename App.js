a5c07861e97db301e45cc6fee2201ed5956b8644
Neesu Rana <rananisu69@gmail.com>
2024-11-25 12:00:23 +0545

    initialized project

---
+++ App.js
@@ -0,0 +1,10 @@
+const child = React.createElement(
+    'div',
+ {id:'child'},
+ [React.createElement('h1', {id: 'heading' }, 'Hello World' ),
+ React.createElement('h2', {id: 'heading2' }, 'Hello World2' ),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+root.render(child);
