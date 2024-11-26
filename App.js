c1de8129299f48ae2057b300c42c21327326e8e2
Neesu Rana <rananisu69@gmail.com>
2024-11-26 13:23:35 +0545

    setup created of React from Scratch

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const child = React.createElement(
-    'div',
- {id:'child'},
- [React.createElement('h1', {id: 'heading' }, 'Hello World' ),
- React.createElement('h2', {id: 'heading2' }, 'Hello World2' ),
-]);
+
+// const child = <div > This is div tag.</div>  //jsx syntax (html like syntax)
+const child = (<div id="container">
+    <h1>Hellow World!</h1>
+    <h2> Hello</h2>
+</div>
+);
+
+//Functional Component
+const Homepage= () =>{
+    return (
+    <div id='container'>
+        <Navbar />
+        <Main />
+        <Footer />
+        {/* <nav> Navbar</nav> */}
+        {/* <main> Main Section</main>
+        <footer> Footer</footer> */}
+    </div>
+)
+    };
+
+// function Navbar() {
+//     return <nav> It is  navbar.</nav>
+// }
+const Navbar= () =>{
+    return <nav>This is navbar.</nav>;
+};
+
+const Footer=()=>{
+    return(
+        <footer>
+            <p> This is footer.</p>
+        </footer>
+    )
+};
+
+const Main=()=>{
+    return(
+        <main>
+            <p> This is main section.</p>
+        </main>
+    )
+};
+
+// const child = React.createElement(
+//     'div',
+//  {id:'child'},
+//  [React.createElement('h1', {id: 'heading' }, 'Hello World' ),
+//  React.createElement('h2', {id: 'heading2' }, 'Hello World2' ),
+
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
-root.render(child);
+// root.render(child); //function render hunna
+root.render(<Homepage/>);
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-Parcel
+# Parcel
 -Makes development build of our project
 Hosts the development onto the server.
 -HMR (Hot Module Replacement)
@@ -7,3 +7,37 @@ Hosts the development onto the server.
 -Minification of File
 -Bundling 
 -Compressing
+
+# JSX
+-html like syntax
+
+
+# Job of parcel
+-Transpilling JSX code
+-Parcel does this transpilling job with the help of external package itself which is Babel.
+
+# Babel
+-Babel is a javascript compiler.
+
+# React element 
+
+const child = (<div id="container">
+    <h1>Hellow World!</h1>
+    <h2> Hello</h2>
+</div>
+);
+-This child is react element. This is not a div tag itself.
+
+# React component
+<!-- component->chunk of code -->
+i. Functional Component- aaile use garney component modern days
+-Functional component is a normal JS function which return jyx
+
+ii. Class based Component- functional aauna vanda aghi use vako
+
+
+# How to create a react app(template)
+
+-Using command:
+npx create-react-app my-app-name
+
